f23214d3cd453377934391481318fdb4030a21ed
Lloyd <lloyd@domh.net>
2020-05-05 15:36:22 +0200

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        colors: {
+            blueh23: '#320aff',
+            pinkh23: '#bb0be2'
+        },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
